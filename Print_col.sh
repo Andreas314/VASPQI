199faf053f4a6857f5964f90a65850b4bfbd69b1
Ono <ondraFaiman@atlas.cz>
2025-08-12 10:11:28 +0200

    Fix a typo in the printing script

--- Print_col.sh
+++ Print_col.sh
@@ -3,7 +3,7 @@ result=0
 for ii in {2..55}
 do
 	infile="$(cat $1 | head -n 1 | tr -s ' '| cut -d ' ' -f $ii)"
-	if [[ "$infile" = "$2" ]]
+	if [[ "$infile" = "$2_real" ]]
 	then
 		result=$ii
 		break
@@ -14,5 +14,7 @@ then
 	echo "Error: Index not found!"
 	exit 1
 fi
-cat $1 | tr -s ' '| cut -d ' ' -f 1,$result
+next="$(( $result + 1 ))"
+cat $1 | tr -s ' '| cut -d ' ' -f 1,$result,$next
 exit 0
+
